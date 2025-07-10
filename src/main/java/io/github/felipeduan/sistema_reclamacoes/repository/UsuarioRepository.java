554c942f866e9b92991e591a10ec27f879307cff
FelipeDuan <felipeduan1@gmail.com>
2025-07-10 20:47:52 -0300

    feat: create Usuario Repository

---
+++ src/main/java/io/github/felipeduan/sistema_reclamacoes/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package io.github.felipeduan.sistema_reclamacoes.repository;
+
+import io.github.felipeduan.sistema_reclamacoes.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByCpf(String cpf);
+
+}
