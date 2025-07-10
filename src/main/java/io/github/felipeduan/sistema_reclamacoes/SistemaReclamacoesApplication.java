592360d4f74a22816076f6d415a8d9bc088d04b9
FelipeDuan <felipeduan1@gmail.com>
2025-07-10 12:29:47 -0300

    init: criando repositório

---
+++ src/main/java/io/github/felipeduan/sistema_reclamacoes/SistemaReclamacoesApplication.java
@@ -0,0 +1,13 @@
+package io.github.felipeduan.sistema_reclamacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReclamacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReclamacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/felipeduan/sistema_reclamacoes/SistemaReclamacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.felipeduan.sistema_reclamacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReclamacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
