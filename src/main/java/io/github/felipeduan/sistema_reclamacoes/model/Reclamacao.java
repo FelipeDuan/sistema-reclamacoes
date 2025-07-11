09bf24dd830cc28ca1e0c83420d18061e433f9e0
FelipeDuan <felipeduan1@gmail.com>
2025-07-11 00:58:26 -0300

    feat: ajuste dos atributos em Reclamacao Model

--- src/main/java/io/github/felipeduan/sistema_reclamacoes/model/Reclamacao.java
+++ src/main/java/io/github/felipeduan/sistema_reclamacoes/model/Reclamacao.java
@@ -19,21 +19,26 @@ public class Reclamacao {
     @Column(columnDefinition = "uuid", updatable = false, nullable = false)
     private UUID id;
 
+    @Column(nullable = false)
     private String titulo;
+
+    @Column(nullable = false)
     private String descricao;
+
+    @Column(nullable = false)
     private LocalDateTime dataCriacao;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private StatusReclamacao status;
 
-    @ManyToOne
-    @JoinColumn(name = "usuario_id")
-    private Usuario usuario;
+    @Column(nullable = false)
+    private String cpfUsuario;
 
-    public Reclamacao(String titulo, String descricao, Usuario usuario) {
+    public Reclamacao(String titulo, String descricao, String cpfUsuario) {
         this.titulo = titulo;
         this.descricao = descricao;
-        this.usuario = usuario;
+        this.cpfUsuario = cpfUsuario;
         this.status = StatusReclamacao.PENDENTE;
         this.dataCriacao = LocalDateTime.now();
     }
